d58cac592ebd40a48b8c5d7165f9851cb9005021
elyl <elylll@gmail.com>
2013-05-22 13:08:46 +0200

    Correction mouvements

--- Mouvements.java
+++ Mouvements.java
@@ -1,10 +1,10 @@
 public class Mouvements
 {
-    public static final Mouvement RIGHT = new Mouvement(1, 0);
-    public static final Mouvement LEFT = new Mouvement(-1, 0);
-    public static final Mouvement AHEAD = new Mouvement(0, 1);
+    public static final Mouvement RIGHT = new Mouvement(0, 1);
+    public static final Mouvement LEFT = new Mouvement(0, -1);
+    public static final Mouvement AHEAD = new Mouvement(1, 0);
     public static final Mouvement RIGHT_AHEAD = new Mouvement(1, 1);
-    public static final Mouvement LEFT_AHEAD = new Mouvement(-1, 1);
+    public static final Mouvement LEFT_AHEAD = new Mouvement(1, -1);
 
     public static final Mouvement NO_MOVE[] = {};
     public static final Mouvement SQUARE_MOVE[] = {AHEAD, LEFT, RIGHT};
