523bc4170fab3487be103e1a715a6d21210e3013
elyl <elylll@gmail.com>
2013-06-08 18:37:30 +0200

    oubli

--- JoueurHasardPur.java
+++ JoueurHasardPur.java
@@ -3,6 +3,11 @@ import java.util.ArrayList;
 /** Gestion de l'IA du joueur adverse. */
 public class JoueurHasardPur extends Joueur
 {
+    public JoueurHasardPur(int couleur)
+    {
+	super(couleur);
+    }
+
     /** Effectue un coup au hasard. */
     public Coup coupChoisi()
     {
