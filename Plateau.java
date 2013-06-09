8f25573315b934ff0c03b95e9bf8de79d22255e8
root <root@localhost.localdomain>
2013-06-09 10:56:47 +0200

    Plateau optimisé

--- Plateau.java
+++ Plateau.java
@@ -3,7 +3,7 @@ public class Plateau
 {
     public static Piece plateau[][];
 
-    /** Initialise un plateau de jeu vide. */
+    /** Genere un plateau de jeu vide. */
     public Plateau()
     {
 	Plateau.plateau = new Piece[8][8];
@@ -12,74 +12,45 @@ public class Plateau
     /** Initialise le plateau de jeu courant. */
     public void initialize()
     {
-	int	i;
-	int	j;
+	int n = 0;
 
-	i = 0;
-	while (i < 8)
-	    {
-		j = 0;
-		while (j < 8)
-		    {
-			if (i == 6)
-			    if (j % 2 == 1)
-				Plateau.plateau[i][j] = new PieceCarree(i, j, Piece.NOIR);
-			    else
-				Plateau.plateau[i][j] = new PieceRonde(i, j, Piece.NOIR);
-			else if (i == 7)
-			    if (j % 2 == 1)
-				Plateau.plateau[i][j] = new PieceRonde(i, j, Piece.NOIR);
-			    else
-				Plateau.plateau[i][j] = new PieceCarree(i, j, Piece.NOIR);
-			else if (i == 0)
-			    if (j % 2 == 1)
-				Plateau.plateau[i][j] = new PieceCarree(i, j, Piece.BLANC);
-			    else
-				Plateau.plateau[i][j] = new PieceRonde(i, j, Piece.BLANC);
-			else if (i == 1)
-			    if (j % 2 == 1)
-				Plateau.plateau[i][j] = new PieceRonde(i, j, Piece.BLANC);
-			    else
-				Plateau.plateau[i][j] = new PieceCarree(i, j, Piece.BLANC);
-			else
-			    Plateau.plateau[i][j] = new PieceVide(i, j, Piece.VIDE);
-			j++;
-		    }
-		i++;
+	for(int j = 0; j < 8; j++){
+	    for(int i = 0; i < 2; i++){
+		if (n % 2 == 1)
+		    Plateau.plateau[i][j] = new PieceCarree(i, j, Piece.NOIR);
+		else
+		    Plateau.plateau[i][j] = new PieceRonde(i, j, Piece.NOIR);
+		n++;
 	    }
+
+	    for(int i = 2; i < 6; i++){
+		Plateau.plateau[i][j] = new PieceVide(i, j, Piece.VIDE);
+	    }
+	    
+	    for(int i = 6; i < 8; i++){
+		if (n % 2 == 1)
+		    Plateau.plateau[i][j] = new PieceCarree(i, j, Piece.BLANC);
+		else
+		    Plateau.plateau[i][j] = new PieceRonde(i, j, Piece.BLANC);
+		n++;
+	    }
+	    n++;
+	}
     }
 
     /** Affiche le plateau de jeu courant. */
     public String toString()
     {
-	String	str;
-	int	i;
-	int	j;
+	String	str = "";
 
-	str = "";
-	i = 0;
-	while (i < 10)
-	    {
-		j = 0;
-		while (j < 11)
-		    {
-			if (i == 0 || i == 9)
-			    if (j == 0 || j == 10)
-				str = str + " ";
-			    else
-				str = str + "_";
-			else
-			    if (j == 0)
-				str = str + i;
-			    else if (j == 1 || j == 10)
-				str = str + "|";
-			    else
-				str = str + Plateau.plateau[i - 1][j - 2].getCode() + " ";
-			j++;
-		    }
-		str = str + "\n";
-		i++;
-	    }
-	return (str);
+	str += "+ - - - - - - - - +\n";
+	for(int i = 0; i < 8; i++) {
+	    str += "|";
+	    for(int j = 0; j < 8; j++)
+		str += " " + Plateau.plateau[i][j].getCode();
+	    str += " |\n";
+	}
+	str += "+ - - - - - - - - +";
+	return str;
     }
 }
